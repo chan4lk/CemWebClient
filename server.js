79e7d8e14a11f8342cba983945265be32ac38c3b
Chandima Ranaweera <chan4lk@gmail.com>
2018-02-15 22:28:03 +0530

    serve static added

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var serveStatic = require('serve-static')
 var server = express();
-server.use('/', express.static(__dirname + '/'));
-server.listen(process.env.PORT || 8080);
+server.use(serveStatic('./', {'index': ['index.html', 'default.htm']}))
+var port = process.env.port || 3000;
+server.listen(port);
+console.log('listing started on port ' + port);
